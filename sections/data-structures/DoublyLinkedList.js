e59ff6f7510b41295da16024286cbfa395d39ef5
Lucas Prazeres <olucascostadp@gmail.com>
2021-02-01 10:23:08 -0300

    DLL - added the shift and ushift methods + refactor the previous methods

--- sections/data-structures/DoublyLinkedList.js
+++ sections/data-structures/DoublyLinkedList.js
@@ -6,7 +6,7 @@ class Node {
   }
 }
 
-class DoublyLinkedLists {
+class DoublyLinkedList {
   constructor() {
     this.head = null;
     this.tail = null;
@@ -14,7 +14,7 @@ class DoublyLinkedLists {
   }
   push(value) {
     const newNode = new Node(value);
-    if (this.length === 0) {
+    if (!this.length) {
       this.head = newNode;
       this.tail = newNode;
     } else {
@@ -27,7 +27,7 @@ class DoublyLinkedLists {
     return this;
   }
   pop() {
-    if (this.length === 0) return null;
+    if (!this.length) return null;
     const poppedNode = this.tail;
     if (this.length === 1) {
       this.head = null;
@@ -35,11 +35,43 @@ class DoublyLinkedLists {
     } else {
       const newTail = poppedNode.prev;
       newTail.next = null;
-      poppedNode.prev = null;
+      this.tail = newTail;
     }
+    poppedNode.prev = null;
     this.length--;
     return poppedNode;
   }
+  shift() {
+    if (!this.length) return null;
+    const shiftedNode = this.head;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = shiftedNode.next;
+      this.head.prev = null;
+    }
+    shiftedNode.next = null;
+    this.length--;
+    return shiftedNode;
+  }
+  unshift(value) {
+    const newNode = new Node(value);
+    if (!this.length) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.head.prev = newNode;
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
 
-const list = new DoublyLinkedLists();
+const list = new DoublyLinkedList();
+list.unshift(1);
+list.unshift(2);
+list.unshift(3);
+console.log(list)
