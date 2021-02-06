42fdd19d2ac4c701e6962bfb04face3eb73d3930
Lucas Prazeres <olucascostadp@gmail.com>
2021-02-06 13:56:21 -0300

    Hash Table - implemented values and keys methods

--- sections/data-structures/HashTable.js
+++ sections/data-structures/HashTable.js
@@ -2,6 +2,16 @@ class HashTable {
   constructor(size = 53) {
     this.keyMap = new Array(size);
   }
+  _hash(key) {
+    let total = 0;
+    let WEIRD_PRIME = 31;
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
+      let char = key[i];
+      let value = char.charCodeAt(0) - 96;
+      total = (total * WEIRD_PRIME + value) % this.keyMap.length;
+    }
+    return total;
+  }
   set(key, value) {
     const index = this._hash(key);
     if (!this.keyMap[index]) {
@@ -19,14 +29,28 @@ class HashTable {
     }
     return undefined;
   }
-  _hash(key) {
-    let total = 0;
-    let WEIRD_PRIME = 31;
-    for (let i = 0; i < Math.min(key.length, 100); i++) {
-      let char = key[i];
-      let value = char.charCodeAt(0) - 96;
-      total = (total * WEIRD_PRIME + value) % this.keyMap.length;
+  keys() {
+    const keysArr = [];
+    for (let group of this.keyMap) {
+      if (group) {
+        for (let [key,] of group) {
+          if (!keysArr.includes(key)) keysArr.push(key);
+        }
+      }
     }
-    return total;
+    console.log(keysArr);
+    return keysArr;
+  }
+  values() {
+    const valuesArr = [];
+    for (let group of this.keyMap) {
+      if (group) {
+        for (let [, value] of group) {
+          if (!valuesArr.includes(value)) valuesArr.push(value);
+        }
+      }
+    }
+    console.log(valuesArr);
+    return valuesArr;
   }
 }
